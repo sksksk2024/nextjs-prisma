b09f6b9c1c934cf0542c615e5e080677f8501dd3
Alex <cota8091@gmail.com>
2025-06-21 07:37:34 +0300

    i lose temper

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,8 +1,14 @@
 import prisma from '@/lib/prisma';
 import { notFound } from 'next/navigation';
+// Define the type for the params prop manually
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
 
 // params is the id fromt the dynamic route naming: [id] = "1"
-export default async function Post({ params }: { params: { id: string } }) {
+export default async function Post({ params }: PageProps) {
   const { id } = params;
 
   const post = await prisma.post.findUnique({
